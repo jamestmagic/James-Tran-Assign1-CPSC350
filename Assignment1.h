538cf668913173595af185af1791dd59f41b457e
jamestmagic <jamestranformal@gmail.com>
2019-09-13 06:00:56 +0000

    Math ops and dna text test

--- Assignment1.h
+++ Assignment1.h
@@ -6,4 +6,8 @@ class Assignment1{
   public:
     Assignment1(); //constructor
     ~Assignment1(); //destructor
+    void readStrings(string t);
+    double findMean(double total, double lines);
+    double findVariance(double meanDiff, double lines);
+    double findSD(double variance);
 };
